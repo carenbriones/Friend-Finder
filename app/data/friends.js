e65cb2a40eddc289d2bf870f377f096b1cfcb9e6
Caren B <carenpbriones@gmail.com>
2019-10-04 01:18:34 -0700

    Added more users, successfully determines who the new user's best match is

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,18 @@
 var users = [{
-    name: "Caren",
-    selfImage: "img.link",
-    answers: ["1", "2", "3", "1", "2", "3", "1", "2", "3", "1"]
-}]
+        name: "Caren",
+        photo: "img.link",
+        scores: ["1", "2", "3", "1", "2", "3", "1", "2", "3", "1"]
+    },
+    {
+        name: "Frank",
+        photo: "photo",
+        scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    },
+    {
+        name: "Charlie",
+        photo: "photo",
+        scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
+    }
+]
 
 module.exports = users;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,28 @@ module.exports = function(app) {
     })
 
     app.post("/api/friends", function(req, res) {
-        users.push(req.body);
-        return req.body;
+        var newUser = req.body;
+
+        // Finds the best match out of all users
+        // Defaults bestMatch to first user
+        var bestMatch = users[0];
+        // Defaults bestMatchDiff to 50 since 50 is the worst match score
+        var bestMatchDiff = 50;
+        for (var i = 0; i < users.length; i++) {
+            var sum = 0;
+            for (var j = 0; j < 10; j++) {
+                sum += Math.abs(parseInt(newUser.scores[j] - parseInt(users[i].scores[j])));
+            }
+
+            if (sum < bestMatchDiff) {
+                bestMatchDiff = sum;
+                bestMatch = users[i];
+            }
+        }
+
+        // Pushes newUser after bestMatch is found so newUser is not in list of users before test
+        users.push(newUser);
+
+        res.json(bestMatch);
     })
 }
