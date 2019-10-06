11fafb12733d812ea47283b5ccbe91ea60befe48
Caren B <carenpbriones@gmail.com>
2019-10-05 23:22:40 -0700

    Does not allow users to submit incomplete surveys

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,15 +12,19 @@ module.exports = function(app) {
 
         // Defaults bestMatch to first user
         var bestMatch = users[0];
-        // Defaults bestMatchDiff to 50 since 50 is the worst match score
-        var bestMatchDiff = 50;
+        // Defaults bestMatchDiff to 40 since 40 is the worst match score
+        var bestMatchDiff = 40;
 
+        // Loops through each user 
         for (var i = 0; i < users.length; i++) {
+
+            // Gets sum of absolute value of differences between answers
             var sum = 0;
             for (var j = 0; j < 10; j++) {
                 sum += Math.abs(parseInt(newUser.scores[j] - parseInt(users[i].scores[j])));
             }
 
+            // If the sum is lower than the best match difference, users[i] is the new best match
             if (sum < bestMatchDiff) {
                 bestMatchDiff = sum;
                 bestMatch = users[i];
